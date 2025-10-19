ff6478a61287adc2e7947734af8f4fc8bdc52b92
ale99318 <jesusjironrodriguez155@gmail.com>
2025-10-19 16:59:59 -0500

    Update core.js

--- core.js
+++ core.js
@@ -1,25 +1,19 @@
 // core.js
-export const state = {
-  week: 1,
-  season: 1,
-  money: 10000,
-  morale: 75,
-  reputation: 40,
-  club: "",
+export const state = {  // <--- 1. ABRE la caja grande "state"
+
+  week: 1,             // (Contenido de state)
+  money: 10000,        // (Contenido de state)
   
-  stadium: {
-    name: "",
-    capacity: 0,
-    costPerWeek: 0,
-    isOwned: false,
-    level: 1
-  },
+  stadium: {           // <--- 2. ABRE la caja pequeña "stadium"
+     name: "",
+     capacity: 0
+  },                   // <--- 2. CIERRA la caja pequeña "stadium"
   
-  ticketPrice: 15,
-  
-  coach: {
+  coach: {             // <--- 3. ABRE la caja pequeña "coach"
     name: "Ninguno",
     style: "N/A",
     salary: 0
-  },
-};
+  }                    // <--- 3. CIERRA la caja pequeña "coach"
+
+}; // <--- 1. CIERRA la caja grande "state"
+   // (El ; que te daba error iba después de esto)
