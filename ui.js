156c74efecdb3be1b6b7291ef45f76025fa0ac41
ale99318 <jesusjironrodriguez155@gmail.com>
2025-10-19 15:34:08 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,5 +1,5 @@
 // ui.js
-import { state } from './core.js'; // Importa el estado
+import { state } from './core.js'; 
 
 // IDs del HTML
 const elClubName = document.getElementById('display-club-name');
@@ -8,13 +8,20 @@ const elSeason = document.getElementById('display-season');
 const elMoney = document.getElementById('display-money');
 const elMorale = document.getElementById('display-morale');
 
+// --- NUEVOS IDs ---
+const elStadiumName = document.getElementById('display-stadium-name');
+const elStadiumCapacity = document.getElementById('display-stadium-capacity');
+
 // Función que actualiza TODOS los valores en pantalla
 export function renderUI() {
-  // ¡Nueva línea! Actualiza el nombre del club
   elClubName.textContent = state.club; 
-  
   elWeek.textContent = state.week;
   elSeason.textContent = state.season;
   elMoney.textContent = state.money;
   elMorale.textContent = state.morale;
+  
+  // --- NUEVAS LÍNEAS ---
+  // Actualizamos la info del estadio
+  elStadiumName.textContent = state.stadium.name;
+  elStadiumCapacity.textContent = state.stadium.capacity;
 }
