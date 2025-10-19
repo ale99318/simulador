5b3b0687e848c4009068b91e3b9de0243558df72
ale99318 <jesusjironrodriguez155@gmail.com>
2025-10-19 15:24:53 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -2,6 +2,7 @@
 import { state } from './core.js'; // Importa el estado
 
 // IDs del HTML
+const elClubName = document.getElementById('display-club-name');
 const elWeek = document.getElementById('display-week');
 const elSeason = document.getElementById('display-season');
 const elMoney = document.getElementById('display-money');
@@ -9,6 +10,9 @@ const elMorale = document.getElementById('display-morale');
 
 // Función que actualiza TODOS los valores en pantalla
 export function renderUI() {
+  // ¡Nueva línea! Actualiza el nombre del club
+  elClubName.textContent = state.club; 
+  
   elWeek.textContent = state.week;
   elSeason.textContent = state.season;
   elMoney.textContent = state.money;
