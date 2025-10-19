708edde1edf2f3752d8f75a5d40cbe6b3f372f8a
ale99318 <jesusjironrodriguez155@gmail.com>
2025-10-19 16:45:01 -0500

    Add getAvailableCoaches function to staff.js

---
+++ staff.js
@@ -0,0 +1,8 @@
+// staff.js
+export function getAvailableCoaches() {
+  return [
+    { id: 'c1', name: 'Manuel Silva (Defensivo)', formation: '5-3-2', salary: 3000 },
+    { id: 'c2', name: 'Javier Mendoza (Equilibrado)', formation: '4-4-2', salary: 3500 },
+    { id: 'c3', name: 'Carlos Rivas (Ofensivo)', formation: '4-3-3', salary: 4500 }
+  ];
+}
